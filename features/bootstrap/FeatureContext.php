2abb4f23fbd374b5a9981637609d5eb281cd159f
Yoann Gesquiere <yoann.gesquiere@gmail.com>
2014-11-16 22:51:02 +0100

    Behat: change from goutte to symfony2 extension

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,15 +1,17 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+class FeatureContext extends MinkContext implements Context, KernelAwareContext
 {
     /**
      * Initializes context.
@@ -18,8 +20,9 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
-    public function __construct()
+    public function __construct(Session $session)
     {
+        $this->session = $session;
     }
 
     protected function unescape($value)
@@ -31,11 +34,18 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
         return str_replace('"', '""', $value);
     }
 
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
     /**
      * @When /^I fill:$/
      */
     public function iFill(TableNode $table)
     {
+        echo '%mink.base_url%';
+        //var_dump($this->getMinkParameter('base_url'));
         foreach ($table->getRowsHash() as $key => $value) {
             $this->fillField($this->escape($key), $this->escape($value));
         }
