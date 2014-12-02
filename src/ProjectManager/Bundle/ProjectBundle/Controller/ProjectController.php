5be17941f9a718f608ba05921d52f236d81d0740
Yoann Gesquiere <yoann.gesquiere@gmail.com>
2014-12-03 00:02:21 +0100

    [Projects] CRUD

--- src/ProjectManager/Bundle/ProjectBundle/Controller/ProjectController.php
+++ src/ProjectManager/Bundle/ProjectBundle/Controller/ProjectController.php
@@ -6,6 +6,7 @@ use ProjectManager\Bundle\CoreBundle\Controller\AbstractController;
 use ProjectManager\Bundle\ProjectBundle\Entity\Project;
 use ProjectManager\Bundle\ProjectBundle\Form\Type\ProjectType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProjectController extends AbstractController
 {
@@ -31,13 +32,63 @@ class ProjectController extends AbstractController
     /**
      * @Template
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        $form = $this->createForm(new ProjectType(), new Project())
+        $project = new Project();
+        $form = $this->createForm(new ProjectType(), $project)
             ->add('save', 'submit');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $project = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($project);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl(self::BASE_URL.'_list'));
+        }
 
         return array(
             'form' => $form->createView(),
         );
+    }
+
+    /**
+     * @Template
+     */
+    public function editAction(Request $request, $id)
+    {
+        $repository = $this->getRepository(self::REPOSITORY_NAME);
+        $project = $repository->find($id);
+        if(!$project) {
+            return $this->redirect($this->generateUrl(self::BASE_URL.'_add'));
+        }
+        $form = $this->createForm(new ProjectType(), $project)
+            ->add('save', 'submit');
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $project = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($project);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl(self::BASE_URL.'_list'));
+        }
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $project = $em->getRepository(self::REPOSITORY_NAME)->find($id);
+        $em->remove($project);
+        $em->flush();
+        return $this->redirect($this->generateUrl(self::BASE_URL.'_list'));
     }
 }
---
+++ src/ProjectManager/Bundle/ProjectBundle/Controller/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoann
+ * Date: 02/12/14
+ * Time: 23:41
+ */
+
+namespace ProjectManager\Bundle\ProjectBundle\Controller;
+
+
+class TaskController {
+
+} 
