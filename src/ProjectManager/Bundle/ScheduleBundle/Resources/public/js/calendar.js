1ca5e9202981a5056410dff85f96b09ca7858e74
Yoann Gesquiere <yoann.gesquiere@gmail.com>
2014-12-14 22:52:38 +0100

    [Core] Add JQuery

--- src/ProjectManager/Bundle/ScheduleBundle/Resources/public/js/calendar.js
+++ src/ProjectManager/Bundle/ScheduleBundle/Resources/public/js/calendar.js
@@ -1,12 +1,31 @@
 function addEvent(obj, event, fct) {
-    if (obj.attachEvent) //Est-ce IE ?
-        obj.attachEvent("on" + event, fct); //Ne pas oublier le "on"
+    if (obj.attachEvent)
+        obj.attachEvent("on" + event, fct);
     else
         obj.addEventListener(event, fct, true);
 }
 
 function updateCalendar(){
 
+    $( ".user_tasks" ).each(function(  ) {
+        //Get user information
+        var divId = $( this ).attr('id');
+        var userId = divId.substring(divId.indexOf('_')+1, divId.lastIndexOf('_'));
+
+        //Get the date for each task
+        var tasksContent = $( this ).html().trim();
+        if ("" != tasksContent)
+        {
+            var tasks = tasksContent.split('\n');
+
+            for (i = 0; i < tasks.length; i++) {
+                alert(tasks[i]);
+                var task = JSON.parse(tasks[i]);
+                alert(task.id);
+            }
+            //Fill the date to identify the task
+        }
+    });
 }
 
 addEvent(window , "load", updateCalendar());
