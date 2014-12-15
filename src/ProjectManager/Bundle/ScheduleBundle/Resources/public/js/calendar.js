e156c8e7c1079728fb202d67e6c6581502e69ec2
Yoann Gesquiere <yoann.gesquiere@gmail.com>
2014-12-15 22:39:11 +0100

    [Schedule] Display task names on calendar

--- src/ProjectManager/Bundle/ScheduleBundle/Resources/public/js/calendar.js
+++ src/ProjectManager/Bundle/ScheduleBundle/Resources/public/js/calendar.js
@@ -16,14 +16,32 @@ function updateCalendar(){
         var tasksContent = $( this ).html().trim();
         if ("" != tasksContent)
         {
+            var columns = $(".week_calendar_header");
+            var columnsData = new Array();
+            for (j = 0; j < columns.length; j++) {
+                var line = (columns[j].classList);
+                //Don't use the users' column
+                if(line.contains("calendar_users"))
+                {
+                    continue;
+                }
+                var columnDate = columns[j].id.substring(6);
+                var whereIsDay = columns[j].className.indexOf("day_")
+                var columnDay = columns[j].className.substring(whereIsDay + 4, whereIsDay + 5);
+                columnsData[columnDay] = columnDate;
+            }
+
             var tasks = tasksContent.split('\n');
 
             for (i = 0; i < tasks.length; i++) {
-                alert(tasks[i]);
                 var task = JSON.parse(tasks[i]);
-                alert(task.id);
+                for (j = 0; j < columnsData.length; j++) {
+                    if (task.start <= columnsData[j] && task.end >= columnsData[j]) {
+                        var td = "tr#user_row_"+userId+ " td.day_"+j;
+                        $(td).html($(td).html()+task.name);
+                    }
+                }
             }
-            //Fill the date to identify the task
         }
     });
 }
