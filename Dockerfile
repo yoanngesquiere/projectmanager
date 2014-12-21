72566fd330d2ca0057c6a897145a03735a93554b
Yoann Gesquiere <yoann.gesquiere@gmail.com>
2014-12-21 23:26:39 +0100

    Docker all the things

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ RUN apt-get install -y curl git-core php5-mysql php5-cli php5-intl
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 RUN apt-get clean
 
-ADD . /var/www/
 WORKDIR /var/www/
--- docker_setup.sh
+++ docker_setup.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+# Get the container address at startup
 sed -i -e "s/database_host:.*/database_host: $PROJECTMANAGER_DB_1_PORT_3306_TCP_ADDR/" app/config/parameters.yml
-chmod -R 777 app/cache
+# Clear cache avoiding permission problems in the new container
+rm -rf app/cache
+# Classic symfony install
 composer install
-php app/console doctrine:database:create -vvv
-php app/console doctrine:schema:create -vvv
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
+php app/console doctrine:schema:create
+# Container is with root user, application with www-data
 chmod -R 777 app/cache
