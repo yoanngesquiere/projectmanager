e89793c8cf639be16c24e09c1da8b041d84b309d
Yoann Gesquiere <ygesquiere@auchan.net>
2015-01-28 13:47:10 +0100

    Add Node, and node dependencies

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM eboraas/apache-php:latest
 RUN apt-get update
 RUN apt-get install -y curl git-core php5-mysql php5-cli php5-intl
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+RUN curl -sL https://deb.nodesource.com/setup | bash -
+RUN apt-get install -y nodejs nodejs-legacy
 RUN apt-get clean
 
 WORKDIR /var/www/
--- docker_setup.sh
+++ docker_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Symfony Installation : Start"
 # Get the container address at startup
 sed -i -e "s/database_host:.*/database_host: $PROJECTMANAGER_DB_1_PORT_3306_TCP_ADDR/" app/config/parameters.yml
 # Clear cache avoiding permission problems in the new container
@@ -10,3 +11,11 @@ php app/console doctrine:database:create
 php app/console doctrine:schema:create
 # Container is with root user, application with www-data
 chmod -R 777 app/cache
+echo "Symfony Installation : End"
+
+# Install FrontEnd dependencies
+echo "FrontEnd dependencies Installation : Start"
+npm install
+echo "FrontEnd dependencies Installation : End"
+
+echo "Installation Complete"
